0e213394a4125068f008bba33235f1e16766886a
Bhumil Sarvaiya <sarvaiyabhumil@gmail.com>
2020-06-10 14:14:59 +0530

    v1.0.1

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ extras_require = {
 
 setup(
     name="supertokens_flask",
-    version="1.0.0",
+    version="1.0.1",
     author="SuperTokens",
     license="Apache 2.0",
     author_email="team@supertokens.io",
